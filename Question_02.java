f17f504667aa3571a8c823a4c52abdeb898c03be
Ata <atadurweshziyaee@gmail.com>
2023-01-25 20:53:12 +0530

    Write a java program to print the sum of two numbers

---
+++ Question_02.java
@@ -0,0 +1,7 @@
+class Question_02 {
+    public static void main(String args[]) {
+        int num1 = 25, num2 = 25;
+        int num3 = num1 + num2;
+        System.out.println("The sum of two numbers is " + num3);
+    }
+}
