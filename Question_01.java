102f256fbeb9c30e985b8cae78b56822fc2f5308
Ata <atadurweshziyaee@gmail.com>
2023-01-25 20:44:54 +0530

    write a java program to print 'hello world' on screen.

---
+++ Question_01.java
@@ -0,0 +1,6 @@
+//Write a java program to print 'Hello world!' on screen.
+class Question_01{
+    public static void main(String args[]){
+        System.out.println("Hello world!"); // for printing
+    }
+}
