51752c5dd413902b1c2f27e4b692604ff6b60f9d
Ata <atadurweshziyaee@gmail.com>
2023-01-25 21:30:16 +0530

    Write a prog to print add,sub,multi,divi,rem

---
+++ Question_04.java
@@ -0,0 +1,23 @@
+
+// Write a Java prog to print the addition, multiplication, subtraction, division and remainder of two numbers.
+import java.util.Scanner;
+
+class Question_04 {
+    public static void main(String args[]) {
+        float num1, num2;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first input :-");
+        num1 = scan.nextFloat();
+        System.out.println("Enter the second input :-");
+        num2 = scan.nextFloat();
+        // Addition, Subtraction, Multiplication, Division, Remainder.
+
+        System.out.println("The addition of the two numbers is " + num1 + "+" + num2 + "=" + (num1 + num2));
+        System.out.println("The subtraction of the two numbers is " + num1 + "-" + num2 + "=" + (num1 - num2));
+        System.out.println("The multiplication of the two numbers is " + num1 + "*" + num2 + "=" + (num1 * num2));
+        System.out.println("The division of the two numbers is " + num1 + "/" + num2 + "=" + (num1 / num2));
+        System.out.println("The remainder of the two numbers is " + num1 + "%" + num2 + "=" + (num1 % num2));
+
+    }
+}
